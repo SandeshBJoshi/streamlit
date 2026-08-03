3832265a5033a01020c9f1fc99b1e5bdaa4fe2bf
Sandesh Joshi <145898739+SandeshBJoshi@users.noreply.github.com>
2026-08-03 10:37:12 +0530

    Fix variable assignment for name input

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 st.title("king gavade.com")
-name.st.text_input("Enter your name")
+name=st.text_input("Enter your name")
 if st.button("submit"):
   st.write(f"Hello {name}")
